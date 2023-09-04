5d6b334f03370e1de64fb4ed753200eb65f1b3c2
Bacteria007 <123715803+Bacteria007@users.noreply.github.com>
2023-09-04 06:20:55 +0500

    catch lgaya

--- src/screens/Auth/LogInScreen.js
+++ src/screens/Auth/LogInScreen.js
@@ -54,10 +54,13 @@ const LogInScreen = ({ navigation }) => {
     messaging()
       .getToken()
       .then(token => {
-        console.log('FCM Token:', token);
+        console.log('FCM Token***&&&&&&&&&&&&&&&&&&&&&&&:', token);
         setFcmToken(token); // Set the fcmToken state
         // Send this token to your backend to associate it with the user.
-      });
+      }).catch((err)=>{
+        console.log('err in FCM Token login***&&&&&&&&&&&&&&&&&&&&&&&:', err);
+        
+      })
   }, []);
 
 
